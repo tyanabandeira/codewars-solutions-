b092e1c552cd684f06949e0f54711ca604de3824
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-10 11:11:25 -0400

    pushing codewar

---
+++ evenOrOdd.js
@@ -0,0 +1,25 @@
+function evenOrOdd(number) {
+  if(number % 2 === 0){
+    return "Even"
+  }else{
+    return "Odd"
+  }
+}
+
+// return number % 2 === 0 ?  "Even" : "Odd"
+
+
+/* PREP
+
+Parameters: numbers 
+--> operations??
+
+Return:even for "Even" numbers, odd for "Odd" numbers
+
+Example: (2) "Even" (3) "Odd"
+
+Psuedocode:
+ --> if/conditional 
+ --> an operation either % or /
+ --> all even numbers are divisble by 2 
+ */
