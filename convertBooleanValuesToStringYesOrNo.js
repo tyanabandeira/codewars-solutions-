8841dd504c089a5d5728289af722d443de95c5e5
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-10 11:59:58 -0400

    pushing codewar

---
+++ convertBooleanValuesToStringYesOrNo.js
@@ -0,0 +1,28 @@
+function boolToWord( bool ){
+  if (bool === true){
+    return "Yes"
+  }else{
+    return "No"
+  }
+}
+
+// return bool === true ? "Yes" : "No"
+
+/* PREP
+
+Parameter:bool (boolean)
+
+Return: a "Yes" for true, "No" for false
+
+Example:
+[true] --> "Yes"
+[false] --> "No"
+
+Psuedocode:
+1. method that takes a boolean value 
+FUNCTION
+2. return a "Yes" string for true, or a "No" string for false
+IF/ELSE 
+
+
+*/
