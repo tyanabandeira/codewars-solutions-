268652179039e2930c26176e217d27c1eee1b639
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-10 11:26:05 -0400

    pushing codewar

---
+++ convertANumberToAString.js
@@ -0,0 +1,27 @@
+function numberToString(num) {
+
+  return num.toString()
+  
+}
+
+
+
+/* 
+
+PREP
+
+Parameter: num (number)
+
+Return: a string of a number 
+
+Example:
+[2] --> "2"
+[50] --> "50"
+
+
+Pseudocode:
+1. We need a function
+2. that can transform a number (integer) into a string.
+.toString()
+
+*/
