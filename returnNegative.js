cb29a17a83d38c5f1fb91be3d8dd6c94de798d51
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-10 22:58:14 -0400

    pushing codewar

---
+++ returnNegative.js
@@ -0,0 +1,26 @@
+function makeNegative(num) {
+  if(num <= 0){
+    return num
+  }else{
+    return -num
+  }
+}
+
+/*
+
+ PREP
+ 
+ Parameter: num
+ 
+ Return: any given number negative
+ 
+ Example:
+ [-2] --> -2
+ [5] --> -5
+ 
+ Psuedocode:
+ 1. given a number (parameter)
+ If conditional
+ 2. return the given number negative
+
+ */
