6f14974f23d829e52723e625c3f450630fc0a362
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-14 09:23:18 -0400

    pushing codewar 8.14.23

---
+++ convertABooleanIntoAString.js
@@ -0,0 +1,32 @@
+function booleanToString(b){
+  if(b === true){
+    return "true"
+  }else{
+    return "false"
+  }
+  
+}
+
+
+/*
+
+PREP
+
+
+Parameter: b which is a boolean
+
+
+Return: a boolean string (turning the boolean into a string)
+
+
+Example: 
+true --> "true"
+false --> "false"
+
+Psuedocode:
+1. Implement a function
+function was created already
+2. Convert the boolean value into its string representation
+IF/ELSE
+
+*/
