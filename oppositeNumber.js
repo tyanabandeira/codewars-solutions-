53e7e6f17c293e7aa197489689258f3330ac7620
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-14 10:00:05 -0400

    pushing codewar #3 8.14.23

---
+++ oppositeNumber.js
@@ -0,0 +1,30 @@
+function opposite(number) {
+  
+  if(number){
+    return -number
+  }else{
+    return number 
+  }
+  
+}
+
+/* 
+
+PREP
+
+Parameter: number (any number)
+
+Return: the opposite of that number
+
+Example:
+[9] --> -9
+[-9] --> 9
+
+Pseudocode:
+1. Given an integer
+parameter is any number
+2. Find its opposite 
+IF/ELSE
+
+
+*/
---
+++ reversedString.js
@@ -0,0 +1,27 @@
+function solution(str){
+  return str.split('').reverse().join('')
+}
+
+
+/*
+
+PREP
+
+Parameter: str (a string)
+
+Return: the string reversed
+
+Example:
+[dog] --> god
+[cat] --> tac
+
+Psuedocode:
+1. Create a function
+function is already created 
+2. Reverse the string passed into it 
+.join()
+.split()
+.reverse()
+
+
+*/
