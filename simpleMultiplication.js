c98ed207e6c7c3247b335441b8c91fd3192d7e9b
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-16 16:55:38 -0400

    pushing codewar #4 8.16.23

---
+++ simpleMultiplication.js
@@ -0,0 +1,27 @@
+function simpleMultiplication(number) {
+  if(number % 2 === 0){
+    return number * 8
+  }else{
+    return number * 9
+  }
+}
+
+
+/*
+
+PREP
+
+Parameter: number 
+
+Return: return a number multiplied by 8(even) or 9(odd)
+
+Example:
+[2] --> 2 * 8
+[3] --> 3 * 9
+
+Pseudocode:
+1. Given a number, any number 
+2. Multiply by 8, if it is an even number, multiply byb 9 otherwise
+IF/ELSE or ternanry 
+
+*/
