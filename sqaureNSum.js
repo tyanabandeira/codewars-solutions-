df3d4f83f4bf2dfdffd2e9f18b1872499980acc7
tyana bandeira <tyanabandeira1@gmail.com>
2023-08-30 21:44:19 -0400

    pushing codewar Square(n)Sum 8kyu

---
+++ sqaureNSum.js
@@ -0,0 +1,10 @@
+function squareSum(numbers){
+  let sum = 0
+  for(let i = 0; i < numbers.length; i++){
+//     numbers[i] ** 2
+  sum += numbers[i] ** 2
+//      return sum += numbers[i] ** 2
+  }
+ return sum
+}
+  
